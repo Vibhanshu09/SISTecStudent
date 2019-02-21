e37da9770d02b1b70f7be01dfa930ce54c982a87
Abhilash Pandey <getabhilashp@gmail.com>
2019-02-21 18:14:49 +0530

    Change password layout done

--- app/src/main/java/com/sistec/sistecstudents/ChangePassword.java
+++ app/src/main/java/com/sistec/sistecstudents/ChangePassword.java
@@ -2,12 +2,98 @@ package com.sistec.sistecstudents;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ChangePassword extends AppCompatActivity {
 
+    EditText con_passEditText, old_passEditText,new_passEditText;
+    Button changepasswordBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        con_passEditText = findViewById(R.id.con_pass);
+        old_passEditText = findViewById(R.id.old_pass);
+        new_passEditText = findViewById(R.id.new_pass);
+        changepasswordBtn = findViewById(R.id.change_pass_btn);
+
+
+        con_passEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+             }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() > 4 && con_passEditText.getText().toString().trim().length() > 4 && old_passEditText.getText().toString().trim().length() > 4 && new_passEditText.getText().toString().trim().length() > 4) {
+                activateBtn();
+            } else {
+                deactivateBtn();
+            }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        old_passEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 4 && con_passEditText.getText().toString().trim().length() > 4 && old_passEditText.getText().toString().trim().length() > 4 && new_passEditText.getText().toString().trim().length() > 4) {
+                    activateBtn();
+                } else {
+                    deactivateBtn();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        new_passEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 4 && con_passEditText.getText().toString().trim().length() > 4 && old_passEditText.getText().toString().trim().length() > 4 && new_passEditText.getText().toString().trim().length() > 4) {
+                    activateBtn();
+                } else {
+                    deactivateBtn();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+    private void activateBtn(){
+        changepasswordBtn.setBackground(getResources().getDrawable(R.drawable.button_enabled));
+        changepasswordBtn.setEnabled(true);
+
+    }
+    private void deactivateBtn(){
+        changepasswordBtn.setBackground(getResources().getDrawable(R.drawable.button_disabled));
+        changepasswordBtn.setEnabled(false);
+
     }
 }
